15942abe860536f0f99c876e6bb428938dad67b1
JorgeRodriguez1998 <jorgerodriguez5tos@gmail.com>
2022-04-26 08:50:19 -0500

    Updating Index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Spiderman = require("./app/Spiderman");
+
+const spiderman1 = new Spiderman("Amazing Spiderman", "Andrew Garfield", "Peter Parker");
+
+console.log(spiderman1.name);
+console.log(spiderman1.heroName);
+console.log(spiderman1.actor);
